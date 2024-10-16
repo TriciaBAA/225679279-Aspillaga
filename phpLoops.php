922d07693ea0f29875feb95932df517f6241cbe7
TriciaBAA <pat.aspillaga@gmail.com>
2024-10-17 01:38:25 +0800

    pushing sir prim's cloned repo

---
+++ phpLoops.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP LOOPS ACTIVITY</title>
+</head>
+<body>
+    <?php
+    echo "Activity 1: Number Counter";
+    $counter = 2;
+    while ($counter <=20) {
+    echo $counter."";
+    $counter+=2;
+    }
+    ?>    
+
+    <?php
+    echo "Activity 2: Password Validator";
+    ?>
+
+
+</body>
+</html>
